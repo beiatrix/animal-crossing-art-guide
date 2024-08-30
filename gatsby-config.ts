cbf3df9cb91cf122a7bf85250d56f900452fb1a0
beiatrix <bpedrasa@gmail.com>
2024-08-29 19:54:51 -0500

    chore: init manifest, favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,26 +7,51 @@ require("dotenv").config({
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Animal Crossing Art Guide`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://animal-crossing-art-guide.netlify.app/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      spaceId: process.env.CONTENTFUL_SPACE_ID
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID
+      }
+    }, 
+    "gatsby-plugin-image", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp", 
+    "gatsby-plugin-postcss", 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
     },
-    __key: "images"
-  }]
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Animal Crossing Art Guide",
+        short_name: "Animal Crossing Art Guide",
+        start_url: "/",
+        background_color: "#57cc99",
+        theme_color: "#57cc99",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+  ]
 };
 
 export default config;
