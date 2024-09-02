c9a338f22a8e1ee29198367020b3ac39e8097ee2
beiatrix <bpedrasa@gmail.com>
2024-09-02 13:49:40 -0500

    feat: responsive styling

--- src/components/ArtCard.tsx
+++ src/components/ArtCard.tsx
@@ -22,19 +22,19 @@ const ArtCard = ({ art }: ArtCardProps) => {
 
   return (
     <div className="p-6">
-      <h1 className="text-5xl font-bold text-green-8 pb-4">
+      <h1 className="text-4xl md:text-5xl font-bold text-green-8 pb-4">
         {art.title}
       </h1>
-      <h2 className="font-serif text-green-7 pb-8">
+      <h2 className="font-serif text-green-7 pb-6 md:pb-8">
         {art.originalTitle}
       </h2>
-      <div className="rounded-2xl bg-green-1 p-10 mb-4">
-        <div className="flex">
-          <div className="w-1/2 text-center">
+      <div className="rounded-2xl bg-green-1 p-4 md:p-10 mb-4">
+        <div className="flex justify-center">
+          <div className={art.alwaysReal ? 'w-100 text-center' : 'w-1/2 text-center'}>
             {
               <div>
                 <GatsbyImage
-                  className="h-60 w-auto m-2"
+                  className="max-h-60 w-auto m-2"
                   objectFit="scale-down"
                   image={art.realImage?.gatsbyImageData}
                   alt={`Real ${art.title}`}
@@ -48,28 +48,24 @@ const ArtCard = ({ art }: ArtCardProps) => {
               </div>
             }
           </div>
-          <div className="w-1/2 text-center">
+
           {
-              art.alwaysReal
-                ? <div className="h-full w-full place-content-center">
-                    <p className="text-lg text-gray-800 font-extrabold p-3">This artwork is always real</p>
-                    <p className="text-2xl">🎉</p>
-                  </div>
-                : <div>
-                    <GatsbyImage
-                      className="h-60 w-auto m-2"
-                      objectFit="scale-down"
-                      image={art.fakeImage?.gatsbyImageData}
-                      alt={`Fake ${art.title}`}
-                    />
-                    <p className="font-bold font-poppins text-sm uppercase text-gray-700 tracking-wide p-3">
-                      Fake ❌
-                    </p>
-                  </div>
+              !art.alwaysReal && 
+                <div className="w-1/2 text-center">
+                  <GatsbyImage
+                    className="max-h-60 w-auto m-2"
+                    objectFit="scale-down"
+                    image={art.fakeImage?.gatsbyImageData}
+                    alt={`Fake ${art.title}`}
+                  />
+                  <p className="font-bold font-poppins text-sm uppercase text-gray-700 tracking-wide p-3">
+                    Fake ❌
+                  </p>
+                </div>
           }
-          </div>
+
         </div>
-        <p className="text-lg text-green-7 text-center pt-6 px-20">
+        <p className="md:text-lg text-green-7 text-center pt-3 md:pt-6 md:px-10 lg:px-20">
           {getDescription(art)}
         </p>
       </div>
